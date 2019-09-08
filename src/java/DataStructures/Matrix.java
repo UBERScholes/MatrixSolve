139ed94e63406e47e979117dd8727c2228bd7646
Vova <butckevichvova@gmail.com>
2019-09-08 22:50:53 +0300

    Добавление ограничения по значению для ввода в матрицу (только 0 или 1)

--- src/java/DataStructures/Matrix.java
+++ src/java/DataStructures/Matrix.java
@@ -13,13 +13,16 @@ public class Matrix {
         this.data = new int[width][height];
     }
 
-    public void fill_Matrix(int[] input) throws InputDataSizeException {
+    public void fill_Matrix(int[] input) throws InputDataSizeException, IllegalArgumentException {
             if (this.data.length*this.data[0].length != input.length) {
                 throw new InputDataSizeException(this.data.length*this.data[0].length, input.length);
             }
             int count = 0;
             for (int i = 0; i < this.height; i++)
                 for (int j = 0; j < this.width; j++) {
+                    if(input[count]!=0&&input[count]!=1){
+                        throw new IllegalArgumentException("Only 1's and 0's are allowed for input");
+                    }
                     this.data[i][j] = input[count];
                     count++;
                 }
--- tests/DataStructuresTests/MatrixTest.java
+++ tests/DataStructuresTests/MatrixTest.java
@@ -10,13 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class MatrixTest{
 
     @Test
-    void test_fillMatrix() throws InputDataSizeException {
-        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+    void test_fillMatrix() throws InputDataSizeException, IllegalArgumentException {
+        int[][] matrix = new int[][]{{1,1,1},{0,0,0},{1,1,1}};
         Matrix test = new Matrix(3,3);
-        test.fill_Matrix(new int[]{1,2,3,4,5,6,7,8,9});
+        test.fill_Matrix(new int[]{1,1,1,0,0,0,1,1,1});
         assertArrayEquals(matrix, test.getData());
-        assertThrows(InputDataSizeException.class, ()->{test.fill_Matrix(new int[]{1,2,3,4});});
-        assertThrows(InputDataSizeException.class, ()->{test.fill_Matrix(new int[]{1,2,3,4,2,3,4,2,3,4,2,3,4,2,3,4});});
+        assertThrows(InputDataSizeException.class, ()->{test.fill_Matrix(new int[]{1,1,1,1});});
+        assertThrows(InputDataSizeException.class, ()->{test.fill_Matrix(new int[]{1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0});});
+        assertThrows(IllegalArgumentException.class, ()->{test.fill_Matrix(new int[]{1,0,-1,4,1,0,0,0,0});});
     }
    // @Test(expected = InputDataSizeException.class)
   //  public void test_fillMatrixWrongInput(){
